a8276891fa7ea243d7f33ea57d3ee00f24f18312
WebByte Studio <businessmail.001b@gmail.com>
2025-04-15 12:44:51 +0600

    First Commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1744547660;s:7:"authKey";s:64:"c;F,IjU e4 vD+v;oJ+XO=7,c_S|Bp)XV^!Q=l9c0oP+TB<7c>[UX(i]o!74!8f<";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1883;}
